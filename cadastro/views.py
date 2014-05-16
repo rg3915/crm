d17520ae5d9fc4736c5474aea06551c5638c2c6a
rg3915 <rg3915@yahoo.com.br>
2014-05-15 22:30:44 -0300

    reverse_lazy e unipath

--- cadastro/views.py
+++ cadastro/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.views.generic import CreateView, ListView
+from django.core.urlresolvers import reverse_lazy
 
 from cadastro.models import Inscricao
 from cadastro.forms import InscricaoForm
@@ -11,7 +12,7 @@ def home(request):
 class Criar(CreateView):
 	template_name = 'cadastro.html'
 	model = Inscricao
-	success_url = '../flista'
+	success_url = reverse_lazy('lista')
 
 class Lista(ListView):
 	template_name = 'lista.html'
--- crm/settings.py
+++ crm/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+# import os
+# BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+# Unipath aqui
+from unipath import Path
+BASE_DIR = Path(__file__).parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -59,7 +62,8 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR.child('db.sqlite3'), # Unipath aqui
     }
 }
 
